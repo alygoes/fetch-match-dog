bf453c859dd75beb73fba2a35396cef799564e42
Alyssa <alygoes@gmail.com>
2025-01-29 00:29:46 -0500

    feat(dog) Finalizes dog component
    Final dog commit
    
    Issue #8

--- src/components/DogCard.jsx
+++ src/components/DogCard.jsx
@@ -1,28 +1,26 @@
 import Card from "react-bootstrap/Card";
 import { useState } from "react";
 
-const DogCard = ({
-  dog,
-  initialFavorite,
-  setFavoriteDogs
-}) => {
+const DogCard = ({ dog, initialFavorite, setFavoriteDogs, freezeButton }) => {
   const [isFavorite, setIsFavorite] = useState(initialFavorite ? true : false);
   const toggleFavorite = () => {
-    console.log(isFavorite);
     if (isFavorite) {
-      setFavoriteDogs((dogs)=>dogs.filter(favDog=>favDog.id !==dog.id ))
+      setFavoriteDogs((dogs) => ([...dogs.filter((favDog) => favDog !== dog.id)]));
       setIsFavorite(false);
     } else {
-      setFavoriteDogs((dogs)=>([...dogs, dog.id]))
+      setFavoriteDogs((dogs) => ([...dogs, dog.id]));
       setIsFavorite(true);
     }
   };
   return (
-    <Card style={{ width: "18rem", height: "350px" }}>
+    <Card
+      style={{ width: "18rem", height: "350px", padding: "0" }}
+      className="mb-3 text-center"
+    >
       <div className="card-img-top">
         <img
           src={dog.img}
-          style={{ maxWidth: "100%", maxHeight: "150px" }}
+          style={{ width: "100%", height: "200px" }}
           alt={`Dog profile`}
         ></img>
       </div>
@@ -30,16 +28,40 @@ const DogCard = ({
       <div className="card-body">
         <h5 className="card-title">{dog.name} </h5>
         <h6>{dog.breed}</h6>
-        <p>
-          {dog.age} years old {dog.id} in {dog.zip_code}
-        </p>
-        <input
-          id={dog.id}
-          type="checkbox"
-          value={isFavorite ? true : false}
-          onClick={toggleFavorite}
-        ></input>
-        <label htmlFor={dog.id}>Favorite</label>
+        <h6>
+          {dog.age} {dog.age === 1 ? "year" : "years"} old
+        </h6>
+        <h6>{dog.zip_code}</h6>
+        {isFavorite ? (
+          <button
+            onClick={freezeButton ? ()=>{} : toggleFavorite}
+            style={{
+              border: "0",
+              background: "none",
+
+              fontSize: "40px",
+              position: "absolute",
+              bottom: "0px",
+              left: "15px",
+            }}
+          >
+            &#x2665;
+          </button>
+        ) : (
+          <button
+            onClick={toggleFavorite}
+            style={{
+              border: "0",
+              background: "none",
+              fontSize: "40px",
+              position: "absolute",
+              bottom: "0px",
+              left: "15px",
+            }}
+          >
+            &#x2661;
+          </button>
+        )}
       </div>
     </Card>
   );
--- src/views/Dog.jsx
+++ src/views/Dog.jsx
@@ -3,50 +3,57 @@ import { axiosInstance } from "../api/util";
 import Button from "react-bootstrap/Button";
 import DogCard from "../components/DogCard";
 import MultiSelect from "../components/MultiSelect";
-import { Link } from "react-router";
+import ResponsivePaginationComponent from "react-responsive-pagination";
+import { getBreeds, searchDogs } from "../api/dog";
+import { Col, Dropdown, Container } from "react-bootstrap";
+import Navbar from "react-bootstrap/Navbar";
+import RemoveButton from "../components/RemoveButton";
 
 const Dog = () => {
   const [adoptableDogs, setAdoptableDogs] = useState([]);
   const [sortDirection, setSortDirection] = useState("asc");
   const [breeds, setBreeds] = useState([]);
   const [selectedBreeds, setSelectedBreeds] = useState([]);
-  //future  todo max out favorite dogs
   const [favoriteDogs, setFavoriteDogs] = useState([]);
   const [match, setMatch] = useState({});
-  const [showMatch, setShowMatch] = useState(false)
-
-
+  const [showMatch, setShowMatch] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [total, setTotal] = useState(0);
+  const [loadingText, setLoadingText] = useState("Loading...");
 
   useEffect(() => {
-    axiosInstance
-      .get("dogs/breeds")
+    getBreeds()
       .then((res) => {
         setBreeds(res.data);
       })
       .catch(console.error);
   }, []);
 
+
   useEffect(() => {
-    //TODO: if this is a 401, handle that to say login to see dogs
     const breedQueries = selectedBreeds.length
       ? selectedBreeds.map((breed) => `&breeds=${breed}`).join("")
       : "";
-    axiosInstance
-      .get(`dogs/search?sort=breed:${sortDirection}${breedQueries}`)
+    searchDogs(currentPage, sortDirection, breedQueries)
       .then((res) => {
-        return res.data.resultIds;
+        setTotal(res?.data?.total);
+        return res?.data?.resultIds;
       })
       .then((resIds) => {
-        axiosInstance.post("dogs", resIds).then((res) => {
-          setAdoptableDogs(res.data);
-        });
+        axiosInstance
+          .post("dogs", resIds)
+          .then((res) => {
+            setAdoptableDogs(res.data);
+            if (res.data.length === 0) {
+              setLoadingText("There are no dogs, please search again.");
+            }
+          })
+          .catch(console.error);
       });
-  }, [sortDirection, selectedBreeds]);
+  }, [sortDirection, selectedBreeds, currentPage]);
 
-  const toggleSortDirection = () =>
-    sortDirection === "asc"
-      ? setSortDirection("desc")
-      : setSortDirection("asc");
+  const setAscending = () => setSortDirection("asc");
+  const setDescending = () => setSortDirection("desc");
 
   const addBreedFilter = (newBreed) => {
     setSelectedBreeds((arr) => [...arr, newBreed]);
@@ -56,75 +63,123 @@ const Dog = () => {
     setSelectedBreeds((arr) => arr.toSpliced(toRemove, 1));
   };
 
-  const generateMatch = async()=>{
+  const generateMatch = async () => {
+    console.log(favoriteDogs)
     let matchID;
     try {
-      const response = await  axiosInstance.post("/dogs/match", favoriteDogs)
-      console.log(response.data)
-      matchID = response?.data.match
-    }
-    catch (err) {
-      console.error(err)
+      const response = await axiosInstance.post("/dogs/match", favoriteDogs);
+      console.log(response.data);
+      matchID = response?.data.match;
+    } catch (err) {
+      console.error(err);
     }
     if (matchID) {
       try {
-        const response = await  axiosInstance.post("/dogs", [matchID])
+        const response = await axiosInstance.post("/dogs", [matchID]);
         if (response.data) {
-          setMatch(response.data[0])
-          setShowMatch(true)
+          setMatch(response.data[0]);
+          setShowMatch(true);
         }
       } catch (err) {
-        console.error(err)
+        console.error(err);
       }
     }
-  }
+  };
 
   return (
-    <div className="container text-center">
-      <div>
-        <Link to="/login">
-          <Button>Login</Button>
-        </Link>
-      </div>
-      <div className="row">
-        <h1>Go Fetch</h1>
-      </div>
-      <div className="row">
-        <label>Filter dogs by breed</label>
-        <MultiSelect
-          items={breeds}
-          onSelect={addBreedFilter}
-          onRemove={removeBreedFilter}
-          selectedItems={selectedBreeds}
-        />
-      </div>
-      <div className="row">
-        Filters applied: {selectedBreeds.map((breed) => `${breed}`)}
-      </div>
-      <div><Button onClick={generateMatch}>Match me with a dog!</Button></div>
-    {showMatch && <DogCard dog={match}/>}
-    
-      <div>Favorite dogs: {favoriteDogs.map(dog=> `${dog}`)}</div>
-      <div>Adoptable dogs</div>
-      <div>
-        Sorting by breed: {sortDirection}
-        <Button onClick={toggleSortDirection}>
-          {sortDirection === "asc" ? "desc" : "asc"}
-        </Button>
-      </div>
+    <div>
+      <Navbar className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand>Go Fetch!</Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              <Button onClick={generateMatch}>Match me with a dog!</Button>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
 
-      <div className="row">
-        {adoptableDogs.map((dog, i) => (
-          <DogCard
-            dog={dog}
-            key={i}
-            initialFavorite={favoriteDogs.includes(dog.id)}
-            setFavoriteDogs={setFavoriteDogs}
-          />
-        ))}
-      </div>
+      <div className="container ">
+        <h3 className="mt-4 mb-4 text-center">
+          Search adoptable dogs and find your perfect match! &#x2661;
+        </h3>
+        {showMatch && (
+          <div className="mt-4 mb-4">
+            <div className="row justify-content-center">
+              Congratulations! You have a match:
+            </div>
+            <div className="row justify-content-center">
+              <DogCard dog={match} initialFavorite={true} freezeButton />
+            </div>
+          </div>
+        )}
+        <div className="row">
+          <div className="col">
+            <MultiSelect
+              items={breeds}
+              onSelect={addBreedFilter}
+              onRemove={removeBreedFilter}
+              selectedItems={selectedBreeds}
+            />
+          </div>
+          <div className="col">
+            <Dropdown>
+              <Dropdown.Toggle
+                data-bs-theme="light"
+                variant="light"
+              >
+                Sort By Breed {sortDirection === "asc" ? "A-Z" : "Z-A"}
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item onClick={setAscending}>Breed A-Z</Dropdown.Item>
+                <Dropdown.Item onClick={setDescending}>Breed Z-A</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            {selectedBreeds.map((breed) => (
+              <RemoveButton
+                key={breed}
+                breed={breed}
+                removeBreed={removeBreedFilter}
+              />
+            ))}
+          </div>
+        </div>
 
-      <div>pagination goes here</div>
+        <div className="container mt-4">
+          {adoptableDogs.length > 0 ? (
+            <div>
+              <div className="row">
+                {adoptableDogs.map((dog, i) => (
+                  <Col key={i}>
+                    <DogCard
+                      dog={dog}
+                      initialFavorite={favoriteDogs.includes(dog.id)}
+                      setFavoriteDogs={setFavoriteDogs}
+                      favoriteDogs={favoriteDogs}
+                    />
+                  </Col>
+                ))}
+              </div>
+              <div className="mt-2">
+                <ResponsivePaginationComponent
+                  curent={currentPage}
+                  total={total ? total / 25 : 0}
+                  onPageChange={setCurrentPage}
+                />
+              </div>
+            </div>
+          ) : (
+            <div>
+              <h2>{loadingText}</h2>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
